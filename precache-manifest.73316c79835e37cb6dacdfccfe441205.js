57419da1821e5409d28b11ffc8d3ff584990b4f0
Abhishek K <abhishekakade@gmail.com>
2019-04-04 21:22:18 +0530

    Updates

---
+++ precache-manifest.73316c79835e37cb6dacdfccfe441205.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-redux/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "101d8d3065addc79b6de",
+    "url": "/robofriends-redux/static/js/runtime~main.70818729.js"
+  },
+  {
+    "revision": "2d9bb2b2b86a19ed969d",
+    "url": "/robofriends-redux/static/js/main.ce2b867f.chunk.js"
+  },
+  {
+    "revision": "035a17a0f43e0e921eb4",
+    "url": "/robofriends-redux/static/js/2.b3575bda.chunk.js"
+  },
+  {
+    "revision": "2d9bb2b2b86a19ed969d",
+    "url": "/robofriends-redux/static/css/main.9fe6ba90.chunk.css"
+  },
+  {
+    "revision": "035a17a0f43e0e921eb4",
+    "url": "/robofriends-redux/static/css/2.ccadaacb.chunk.css"
+  },
+  {
+    "revision": "f6f4c5d5e65e3234bf560ffc1927115f",
+    "url": "/robofriends-redux/index.html"
+  }
+];
